3ee411f791cd5560501e145be38ec67419f46082
keonoh00 <keonoh00@gmail.com>
2023-05-22 16:37:59 +0900

    Add Date

--- todolist/src/components/Header/Header.js
+++ todolist/src/components/Header/Header.js
@@ -2,18 +2,19 @@ import "./Header.css";
 import React, { useEffect, useState } from "react";
 
 export const Header = () => {
-  const [time, setTime] = useState(new Date().toLocaleTimeString())
+  const [time, setTime] = useState(new Date())
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setTime(new Date().toLocaleTimeString())
+      setTime(new Date())
     }, 1000)
     return () => clearInterval(interval)
   }, [])
 
   return (
     <div class="header-container">
-      <p className="clock">{time}</p>
+      <p className="clock">{time.toLocaleTimeString()}</p>
+      <p className="date">{time.getFullYear()}년 {time.getMonth()}월 {time.getDate()}일</p>
       <p className="title">To Do List</p>
     </div>
   )
