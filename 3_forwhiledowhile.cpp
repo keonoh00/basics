b232c9c92f8b15de8e9f70aa72b255da949d6932
keonoh00 <keonoh00@gmail.com>
2023-06-12 14:04:51 +0900

    For, While, Do While Loops

---
+++ 3_forwhiledowhile.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void)
+{
+  // For Loop
+  for (int i = 0; i <= 5; i++)
+  {
+    printf("Hello For Loop - %d\n", i);
+  }
+
+  // While Loop
+  int j = 0;
+  while (j <= 5)
+  {
+    printf("Hello While Loop %d\n", j);
+    j++;
+  }
+
+  // Do While Loop
+  int k = 0;
+  do
+  {
+    printf("Hello Do While Loop %d\n", k);
+    k++;
+  } while (k <= 5);
+}
