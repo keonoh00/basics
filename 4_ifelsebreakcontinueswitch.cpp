b8424902a1d898f1c97031393a4f665ad32ef6a1
keonoh00 <keonoh00@gmail.com>
2023-06-13 12:44:52 +0900

    Operators

--- 4_ifelsebreakcontinueswitch.cpp
+++ 4_ifelsebreakcontinueswitch.cpp
@@ -16,4 +16,21 @@ int main(void)
   {
     printf("You are a child\n");
   }
+
+  // Operators
+  // ==, !=, >, <, >=, <=
+  // &&, ||, !
+
+  if (age >= 13 && age <= 19)
+  {
+    printf("You are a teenager\n");
+  }
+  else if (age >= 18)
+  {
+    printf("You are an adult\n");
+  }
+  else
+  {
+    printf("You are a child\n");
+  }
 }
