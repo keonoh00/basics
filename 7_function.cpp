ab93a45baaa10ba8f9ff477a3c33d316feeec6e2
keonoh00 <keonoh00@gmail.com>
2023-06-13 13:47:58 +0900

    Function

---
+++ 7_function.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// How to declare the function
+// returntype functionname(parameterType parameter)
+
+// Eg)
+// Declare a function which has float return value and
+// two parameters "num1" and "num2" with both having integer type
+float multiply(int num1, int num2);
+
+// When nothingis returned then void
+void noReturnPrint(int num1, int num2, int multiplied);
+
+int main(void)
+{
+  for (int i = 0; i < 5; i++)
+  {
+    int multiplied = multiply(i, i + 1);
+    noReturnPrint(i, i + 1, multiplied);
+  }
+}
+
+// Define the function which has been declared
+// The return type and parameters should match the declaration
+float multiply(int num1, int num2)
+{
+  float calculation = num1 * num2;
+  return calculation;
+}
+
+void noReturnPrint(int num1, int num2, int multiplied)
+{
+  printf("%d multiplied by %d is %d\n", num1, num2, multiplied);
+}
