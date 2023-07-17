8e8340ffd567036fb4487ed454d4130ff86b9b85
keonoh00 <keonoh00@gmail.com>
2023-07-17 20:27:28 +0900

    Use arguments for GraphQL in ReactJS to build page for movie detail

--- src/routes/Movie.tsx
+++ src/routes/Movie.tsx
@@ -1,5 +1,36 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router-dom";
+
+const GET_MOVIE = gql`
+  query getMovie($movieId: String!) {
+    movie(id: $movieId) {
+      id
+      title
+    }
+  }
+`;
+
 const Movie = () => {
-  return <div>This is details about a movie</div>;
+  const param = useParams();
+  const movieId = param.id;
+
+  // Because we have implemented caching during ApolloClient setup
+  // loading will be false after the first visit
+  const { data, loading, error } = useQuery(GET_MOVIE, {
+    variables: {
+      movieId,
+    },
+  });
+
+  return loading ? (
+    <h1>Loading...</h1>
+  ) : error ? (
+    <h1>Cannot get data of movie id: {movieId}</h1>
+  ) : (
+    <div>
+      Title is <b>{data.movie.title}</b>
+    </div>
+  );
 };
 
 export default Movie;
