c89dd508c63755a31020efa0d8dca0cf2e8ca1a5
keonoh00 <keonoh00@gmail.com>
2023-07-17 19:59:36 +0900

    How to use useApolloClient

--- src/routes/Movies.tsx
+++ src/routes/Movies.tsx
@@ -1,5 +1,45 @@
+import { useEffect, useState } from "react";
+import { useApolloClient, gql } from "@apollo/client";
+
+const ALL_MOVIES = gql`
+  query getAllMovies {
+    allMovies {
+      id
+      title
+    }
+  }
+`;
+
+type Movie = {
+  id: string;
+  title: string;
+};
+
 const Movies = () => {
-  return <div>This is list of movie</div>;
+  const [movies, setMovies] = useState<Movie[]>();
+
+  const client = useApolloClient();
+
+  useEffect(() => {
+    client
+      .query({
+        query: ALL_MOVIES,
+      })
+      .then((result) => {
+        setMovies(result.data.allMovies);
+      });
+  }, [client]);
+
+  return (
+    <div>
+      {movies?.map((item) => (
+        <>
+          <p>MovieID:{item.id}</p>
+          <p>Movie Title: {item.title}</p>
+        </>
+      ))}
+    </div>
+  );
 };
 
 export default Movies;
