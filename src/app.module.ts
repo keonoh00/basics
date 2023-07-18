458951e65378fa9d78976ccf0be99c1674b5e0bc
keonoh00 <keonoh00@gmail.com>
2023-07-18 14:37:58 +0900

    Clean up NestJS src

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
+
+// AppModule is a root module of the application, it ties all the pieces together.
 export class AppModule {}
