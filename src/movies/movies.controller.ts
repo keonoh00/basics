c22c6d8d69a30e9fabe63cb50628e76b8a58d88e
keonoh00 <keonoh00@gmail.com>
2023-07-18 14:51:26 +0900

    Some basic methods

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+  @Get()
+  getAllMovies() {
+    return 'This will return all movies';
+  }
+
+  @Post()
+  createMovie() {
+    return 'This will create a movie';
+  }
+
+  @Get('/:id') // Same as ExpressJS: app.get('/movies/:id')
+  getMovieById(
+    @Param('id') movieId: string, // NestJS should ask for the parameter, it cannot automatically know what the parameter is.
+  ) {
+    return `This will return one movie with the id: ${movieId}`;
+  }
+
+  @Delete('/:id')
+  deleteMovieById(@Param('id') movieId: string) {
+    return `Deleting movie with id: ${movieId}`;
+  }
+
+  // @Put('/:id') // Put is used to update the entire resource.
+  @Patch('/:id') // Patch is used to update only a part of the resource.
+  updateMovieById(@Param('id') movieId: string) {
+    return `Updating movie with id: ${movieId}`;
+  }
+}
