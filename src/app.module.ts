37728bdc87d7c08025c99bf0e304c3629abce9dd
keonoh00 <keonoh00@gmail.com>
2023-07-18 14:38:29 +0900

    Add explanation about controllers and providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,13 @@
 import { Module } from '@nestjs/common';
 
+/*
+NestJS likes to separate the business logic from the controller.
+- Business logic is the code that is responsible for executing a particular task.
+- Controller is the code that is responsible for handling incoming requests and returning the response to the client.
+So in the controller, we only define the function that will be called when the client makes a request.
+The actual business logic is defined in the service. eg. app.service.ts -> AppService class -> getHello() function.
+*/
+
 @Module({
   imports: [],
   controllers: [],
